d6edcf3e1e537881ec41595df559770b87e815a5
malik iscandari <malikiscan@gmail.com>
2019-04-29 19:50:19 -0700

    able to grab the coordinates of teh browser and built API caller function

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,6 +1,50 @@
 import React, {Component} from 'react'
 
 class Info extends Component {
+    constructor(){
+        super()
+    }
+    componentDidMount(){
+        let cachedLat = localStorage.getItem('latitude')
+        let cachedLon = localStorage.getItem('longitude')
+
+        cachedLat ? // if the cached Latitude exists, then
+        this.setCoordsFromLocalStorage(cachedLat, cachedLon) : // set the coordinates from the local storage as the parameters being passed
+        this.getCoords() // if the cached Latitude does not exist, then get the coordinates
+    }
+
+    getCoords(){
+        // in order to get the user's coordinates, we used the Geolocation API
+        if(window.navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position)=>{ // using a callback function, using position as a parameter to set longitudes and latitudes
+                localStorage.setItem('latitude', position.coords.latitude) // once we have the longitude and lat, 
+                localStorage.setItem('longitude', position.coords.longitude) // set these items in teh localStorage object, so it can be referenced the next time the app is used
+                this.callWeatherApi(position.coords.latitude, position.coords.longitude,"geo") // we then pass those coordinates
+                .then(res => this.setState({response: res.express}))
+                .catch(err => console.log(err))
+            }, (error) => {
+                this.setState({
+                    error: error.message,
+                })
+            } 
+            )
+        }
+    }
+
+    callWeatherApi = async (latitude, longitude, location) => {
+        let response = await fetch('/api/weather?latitude=' + latitude + '&longitude=' + longitude + '&location=' + location)
+        let body = await response.json
+
+        if(body.cod == 404){
+            throw Error(body.message)
+        } else 
+
+
+
+
+
+        []
+    }
     render(){
         return(
         <div>

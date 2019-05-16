a49a83a23f6b264a91a748d40a83729eb57020ab
malik iscandari <malikiscan@gmail.com>
2019-05-15 19:37:27 -0700

    second iteration initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Module from './Module';
+
+const API_Key = '653ce490c43338dbcdbca50a175ec00c'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Module />
     </div>
   );
 }
